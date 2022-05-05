35fad00b5ddd1456daaadbc7e351924dc346538c
cvlvxi <cvlvxi@outlook.com>
2022-05-05 21:56:26 +1000

    Add io_handler for quitting logs with message passing (mpsc)

--- src/main.rs
+++ src/main.rs
@@ -2,55 +2,77 @@
 
 mod logging;
 
-use regex::Regex;
-use std::io::Read;
-use std::process::{Command, Stdio};
-use std::time::Duration;
 
-use tokio::time::sleep;
-
-use crate::logging::adb::Adb;
 use crate::logging::adb::strategies::AdbRegexStrategy;
-use crate::logging::Parser;
+use crate::logging::adb::Adb;
 use crate::logging::LineParts;
+use crate::logging::Parser;
 
+use tokio::io::AsyncReadExt;
 use tokio::sync::mpsc;
+use tokio::io::stdin;
+use tokio::io::{AsyncBufReadExt, BufReader, Lines};
 
-async fn sleep_print(msg: &str) {
-    println!("{:?}", msg);
-    sleep(Duration::from_secs(1)).await;
+async fn receive_logs(rx: &mut mpsc::Receiver<LineParts>) {
+    while let Some(i) = rx.recv().await {
+        println!("Got the line parts: {:?}", i);
+    }
 }
 
-async fn get_log(adb: &Adb<AdbRegexStrategy>, tx: mpsc::Sender<LineParts>) {
+async fn io_handler(tx: mpsc::Sender<bool>) {
+    let mut buffer = String::new();
+    let mut stdin = BufReader::new(stdin());
     loop {
-        // Get next part and send 
-        // let val = adb.next().await;
-        // if let Some(&line_part) = val {
-        //     tx.send(&ine_part).await;
-        // }
+        stdin.read_line(&mut buffer).await;
+        if buffer.contains("dog") {
+            println!("here!");
+            tx.send(true).await;
+        }
+        println!("{:?}", buffer);
     }
+}
+
+async fn get_logs(adb: &mut Adb<AdbRegexStrategy>, tx: mpsc::Sender<LineParts>, rx2: &mut mpsc::Receiver<bool>) {
+    println!("Get logs");
+    loop {
+        let res = rx2.try_recv();
+        if let Ok(val) = res {
+            println!("Exiting log!!!!");
+            break;
+        } else {
+        }
+
+        // Get next part and send
+        let val = adb.next().await;
+        if let Some(line_part) = val {
+            tx.send(line_part).await.unwrap();
+        }
+
+    }
+    
+    println!("I have quit!");
 }
 
 #[tokio::main]
 async fn main() {
     let mut adb = Adb::new(AdbRegexStrategy::new());
     let (tx, mut rx) = mpsc::channel::<LineParts>(100);
-    
-    let a = tokio::spawn(async move {
-        loop {
-            sleep(Duration::from_secs(2)).await;
-            println!("From Logging: {:?}", adb.next().await);
+    let (tx2, mut rx2) = mpsc::channel::<bool>(2);
 
-        }
+    let a = tokio::spawn(async move {
+        get_logs(&mut adb, tx, &mut rx2).await;
     });
 
     let b = tokio::spawn(async move {
         loop {
-            sleep_print("Hello from other thing").await;
+            receive_logs(&mut rx).await;
         }
     });
 
-    a.await;
+    let c = tokio::spawn(async move {
+        io_handler(tx2).await;
+    });
 
-
+    // a.await;
+    c.await;
 }
