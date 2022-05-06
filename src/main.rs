5881ee7de713153fe192d7e879641675221edd71
cvlvxi <cvlvxi@outlook.com>
2022-05-06 12:37:23 +1000

    add some basic ui

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,17 @@
 #![feature(generic_associated_types)]
 
 mod logging;
+mod ui;
 
 
 use crate::logging::adb::strategies::AdbRegexStrategy;
 use crate::logging::adb::Adb;
 use crate::logging::LineParts;
 use crate::logging::Parser;
+use crate::ui::start_ui;
+
+use eyre::Result;
+use log::LevelFilter;
 
 use tokio::sync::mpsc;
 use tokio::io::stdin;
@@ -53,8 +58,13 @@ async fn get_logs(adb: &mut Adb<AdbRegexStrategy>, tx: mpsc::Sender<LineParts>, 
 }
 
 #[tokio::main]
-async fn main() {
+async fn main() -> Result<()> {
     let mut adb = Adb::new(AdbRegexStrategy::new());
+
+    // Configure log
+    tui_logger::init_logger(LevelFilter::Debug).unwrap();
+    tui_logger::set_default_level(log::LevelFilter::Debug);
+
     let (tx, mut rx) = mpsc::channel::<LineParts>(100);
     let (tx2, mut rx2) = mpsc::channel::<bool>(2);
 
@@ -72,6 +82,11 @@ async fn main() {
         io_handler(tx2).await;
     });
 
+    let d = tokio::spawn(async move {
+        start_ui().await;
+    });
+
     // a.await;
     c.await;
+    Ok(())
 }
---
+++ src/ui/mod.rs
@@ -0,0 +1,119 @@
+use std::{io::stdout, time::Duration};
+use eyre::Result;
+use tui::{backend::{CrosstermBackend, Backend}, Terminal, Frame, layout::{Layout, Direction, Constraint, Alignment}, widgets::{Paragraph, Block, Borders, BorderType}, style::{Style, Color}, text::{Spans, Span}};
+
+
+
+// pub enum AppReturn {
+//     Exit,
+//     Continue,
+// }
+
+pub async fn start_ui() -> Result<()> {
+    let stdout = stdout();
+    crossterm::terminal::enable_raw_mode();
+    let backend = CrosstermBackend::new(stdout);
+    let mut terminal = Terminal::new(backend)?;
+
+    // User event handler
+    let tick_rate = Duration::from_millis(200);
+
+    loop {
+        // Render
+        terminal.draw(|rect| draw(rect))?;
+
+        // Handle inputs
+        // let result = match events.next().await {
+        //     InputEvent::Input(key) => app.do_action(key).await,
+        //     InputEvent::Tick => app.update_on_tick().await,
+        // };
+        // Check if we should exit
+        // if result == AppReturn::Exit {
+        //     events.close();
+        //     break;
+        // }
+    }
+
+    // Restore the terminal and close application
+    terminal.clear()?;
+    terminal.show_cursor()?;
+    crossterm::terminal::disable_raw_mode()?;
+
+    
+    Ok(())
+    
+}
+
+pub fn draw<B>(rect: &mut Frame<B>)
+where
+    B: Backend,
+{
+    let size = rect.size();
+
+    // Vertical layout
+    let chunks = Layout::default()
+        .direction(Direction::Vertical)
+        .constraints(
+            [
+                Constraint::Length(3),
+                Constraint::Min(10),
+                Constraint::Length(3),
+                Constraint::Length(12),
+            ]
+            .as_ref(),
+        )
+        .split(size);
+
+    // Title
+    let title = draw_title();
+    rect.render_widget(title, chunks[0]);
+
+    // Body & Help
+    let body_chunks = Layout::default()
+        .direction(Direction::Horizontal)
+        .constraints([Constraint::Min(20), Constraint::Length(32)].as_ref())
+        .split(chunks[1]);
+
+    let body = draw_body();
+    rect.render_widget(body, body_chunks[0]);
+
+    // Logs
+    // let logs = draw_logs();
+    // rect.render_widget(logs, chunks[3]);
+}
+
+fn draw_title<'a>() -> Paragraph<'a> {
+    Paragraph::new("Cool Logs")
+        .style(Style::default().fg(Color::LightCyan))
+        .alignment(Alignment::Center)
+        .block(
+            Block::default()
+                .borders(Borders::ALL)
+                .style(Style::default().fg(Color::White))
+                .border_type(BorderType::Plain),
+        )
+}
+
+fn draw_body<'a>() -> Paragraph<'a> {
+    let initialized_text = "Initialized";
+    let loading_text = "Loading...";
+    let sleep_text = format!("Sleep count: {}", 1);
+
+    let tick_text = format!("Tick count: {}", 100);
+    
+    Paragraph::new(vec![
+        Spans::from(Span::raw(initialized_text)),
+        Spans::from(Span::raw(loading_text)),
+        Spans::from(Span::raw(sleep_text)),
+        Spans::from(Span::raw(tick_text)),
+    ])
+    .style(Style::default().fg(Color::LightCyan))
+    .alignment(Alignment::Left)
+    .block(
+        Block::default()
+            // .title("Body")
+            .borders(Borders::ALL)
+            .style(Style::default().fg(Color::White))
+            .border_type(BorderType::Plain),
+    )
+}
